82a7f5c5d5c62d899e734de32442fd70137fbe6f
gitqwerty777 <goohcl777@gmail.com>
2015-10-14 14:39:52 +0800

    main features complete -- the same as output

--- hw2/src/header.h
+++ hw2/src/header.h
@@ -6,6 +6,11 @@ struct symtab{
 	int counter;
 };
 
+typedef struct Comment{//insertComment
+  char* s;
+  struct Comment *next;
+}Comment;
+
 typedef struct symtab symtab;
 symtab * lookup(char *name);
 void insert(char *name);
--- hw2/src/symboltable.c
+++ hw2/src/symboltable.c
@@ -8,6 +8,7 @@
 #define TABLE_SIZE	256
 
 symtab * hash_table[TABLE_SIZE];
+Comment* comment_root, *previusComment;
 extern int linenumber;
 
 int HASH(char * str){
@@ -38,6 +39,19 @@ symtab * lookup(char *name){
   return NULL;
 }
 
+void insertComment(char* comment){
+  Comment* newc = (Comment*)malloc(sizeof(Comment));
+  int len = strlen(comment);
+  newc->s = (char*)malloc(sizeof(char)*len);
+  strncpy(newc->s, comment, len);
+  newc->next = NULL;
+  if(comment_root == NULL){
+    comment_root = newc;
+  } else {
+    previusComment->next = newc;
+  }
+  previusComment = newc;
+}
 
 void insertID(char *name){
   int hash_key;
@@ -69,6 +83,56 @@ void printSym(symtab* ptr)
 {
   printf(" Name = %s \n", ptr->lexeme);
   printf(" References = %d \n", ptr->counter);
+}
+
+void printComment(){
+  Comment* nowc = comment_root;
+  while(nowc != NULL){
+    printf("%s\n", nowc->s);
+    nowc = nowc->next;
+  }
+}
+
+int compstr(const void* ai, const void* bi){
+  symtab* a = *(symtab**)ai;//not a good thing...
+  symtab* b = *(symtab**)bi;
+  //printf("compare %s %s\n", a->lexeme, b->lexeme);
+  return strcmp(a->lexeme, b->lexeme);
+}
+
+void printIDFrequency(){
+  int i;
+  symtab* hashList = NULL, *nowp = NULL;
+  int count = 0;
+  for (i=0; i<TABLE_SIZE; i++){
+    symtab* symptr = hash_table[i];
+    if (symptr != NULL){
+      if(hashList == NULL){
+	hashList = symptr;
+	nowp = symptr;
+	count++;
+      } else {
+	symptr->back = nowp;
+	nowp->front = symptr;
+      }
+      while(nowp != NULL && nowp->front != NULL){
+	nowp = nowp->front;
+	count++;
+      }
+    }
+  }
+  printf("new count = %d\n", count);
+  symtab** newhashtable = (symtab**)malloc(sizeof(symtab*)*count);
+  for (i=0; i<count; i++){
+    newhashtable[i] = hashList;
+    //printf("prei = %d: %s %d\n", i, hashList->lexeme, hashList->counter);
+    hashList = hashList->front;
+  }
+  qsort(newhashtable, count, sizeof(symtab*), compstr);
+  for (i=0; i<count; i++){
+    symtab* symptr = newhashtable[i];
+    printf("i = %d: %s %d\n", i, symptr->lexeme, symptr->counter);
+  }  
 }
 
 void printSymTab()
