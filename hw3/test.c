481133f2a6fe4fa9a9963efbf76ce14531a1aec7
Lin Chih-An <b01705040@ntu.edu.tw>
2015-11-10 13:42:58 +0800

    DEBUG: decl.c

--- hw3/test.c
+++ hw3/test.c
@@ -1,9 +1,12 @@
-int main() {
-        if (k == j) {
-            write("first");
-        } else if (j == k) {
-            write("second");
-        } else {
-            write("third");
-        }
+int ABC;
+float ABCD, AAA = 4.0, KKK = 5.5;
+typedef float GFLOAT;
+int main() 
+{
+	typedef int INT, INT2, INT3;
+	typedef float FLOAT;
+	typedef void VOID;
+	typedef void VOID2, VOID3;
+	INT f[3][3*3-(4+2)/2];
+	FLOAT f2;
 }
